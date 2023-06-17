5eef43ff3e2fca75a192c3d35e4bb71df9391d7f
BenBogan <ben.bogan.6@gmail.com>
2023-06-17 14:13:04 -0400

    switch to quart

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-from flask import Flask, request, jsonify, abort
-from flask_cors import CORS
+from quart import Quart, request, jsonify, abort
+from quart_cors import cors
 import os
 
-app = Flask(__name__)
-CORS(app)
+app = Quart(__name__)
+app = cors(app, allow_origin="https://chat.openai.com")
+
 BASE_DIR = 'D:/Code/GPT/'
 YOUR_DIRECTORY_PATH = BASE_DIR + 'test/'
-# YOUR_DIRECTORY_PATH = "ABC"
+
 @app.route('/', methods=['GET'])
-def list_files():
+async def list_files():
     print(YOUR_DIRECTORY_PATH)
     files = os.listdir(YOUR_DIRECTORY_PATH)
-    # return jsonify({"ABC": "ABC"})
     return jsonify(files)
 
 @app.route('/<path:filename>', methods=['GET', 'POST'])
-def get_or_write_file(filename):
+async def get_or_write_file(filename):
     file_path = os.path.join(YOUR_DIRECTORY_PATH, filename)
 
     if request.method == 'GET':
@@ -26,7 +26,8 @@ def get_or_write_file(filename):
         else:
             abort(404, description="File not found")
     elif request.method == 'POST':
-        data = request.data.decode('utf-8')  # assumes data is sent as text
+        data = await request.get_data()  # get_data() is an async function in Quart
+        data = data.decode('utf-8')
         with open(file_path, 'w') as file:
             file.write(data)
         return f"Successfully wrote to {filename}"
